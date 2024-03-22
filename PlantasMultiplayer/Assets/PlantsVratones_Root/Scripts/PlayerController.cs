beaae9174ab48773bf451ee09eaf10e4fffef263
pablofon <153851706+pablofon@users.noreply.github.com>
2024-03-22 17:10:17 +0100

    colliders y Rb

--- PlantasMultiplayer/Assets/PlantsVratones_Root/Scripts/PlayerController.cs
+++ PlantasMultiplayer/Assets/PlantsVratones_Root/Scripts/PlayerController.cs
@@ -18,17 +18,62 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public float restablishCoolDown = 2f;
     [SerializeField] bool isFacingRight;
+    [SerializeField] PlayerState currentState;
 
-
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        anim = GetComponent<Animator>();
+        playerRb = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+        currentState = PlayerState.normal;
+        isFacingRight = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        FlipUpdater();
+
+        //Lectura del input de movimiento constante SOLO sie el player esta en el estado deseado (estado normal)
+        if (currentState == PlayerState.normal)
+        {
+            horInput = playerInput.actions["Movement"].ReadValue<Vector2>();
+            vertInput = playerInput.actions["Movement"].ReadValue<Vector2>();
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (currentState == PlayerState.normal) { Movement(); }
+    }
+
+    void Movement()
+    {
+        playerRb.velocity = new Vector2(horInput.x * speed, vertInput.y * speed);
+    }
+
+    void Flip()
+    {
+        Vector3 currentScale = transform.localScale;
+        currentScale.x *= -1;
+        transform.localScale = currentScale;
+        isFacingRight = !isFacingRight;
+    }
+
+    void FlipUpdater()
+    {
+        if (horInput.x > 0)
+        {
+            if (!isFacingRight)
+            {
+                Flip();
+            }
+        }
+        if (horInput.x < 0)
+        {
+            if (isFacingRight)
+            {
+                Flip();
+            }
+        }
     }
 }
