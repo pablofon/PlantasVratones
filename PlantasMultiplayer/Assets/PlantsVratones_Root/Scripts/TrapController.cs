d7491b26757105b5414905d0f2ea1c14e4ca19bb
pablofon <153851706+pablofon@users.noreply.github.com>
2024-03-25 20:01:35 +0100

    Ajustes varios

---
+++ PlantasMultiplayer/Assets/PlantsVratones_Root/Scripts/TrapController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapController : MonoBehaviour
+{
+    Animator anim;
+
+    [Header("Trap Stats")]
+    public float speed;
+    public Transform basePosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Attack"))
+        {
+            Bomb();
+        }
+        if (collision.gameObject.CompareTag("Water"))
+        {
+           TrapDeath();
+        }
+    }
+
+    public void Run()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, basePosition.position, speed * Time.deltaTime);
+    }
+
+    public void Bomb()
+    {
+
+    }
+
+    public void TrapDeath()
+    {
+        gameObject.SetActive(false);
+    }
+}
