0025f9c1db2c6f8121a14dff55211124e5171e52
pablofon <153851706+pablofon@users.noreply.github.com>
2024-04-12 16:22:59 +0200

    spawners creados

--- PlantasMultiplayer/Assets/PlantsVratones_Root/Scripts/EnemyController.cs
+++ PlantasMultiplayer/Assets/PlantsVratones_Root/Scripts/EnemyController.cs
@@ -15,7 +15,7 @@ public class EnemyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        basePosition = GameObject.FindGameObjectWithTag("Water").transform;
     }
 
     // Update is called once per frame
---
+++ PlantasMultiplayer/Assets/PlantsVratones_Root/Scripts/SpawnManager.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] enemyPrefabsOne;
+    [SerializeField] GameObject[] enemyPrefabsTwo;
+    [SerializeField] GameObject[] enemyPrefabsThree;
+    [SerializeField] Transform[] spawners;
+    [SerializeField] float castTime;
+    [SerializeField] float repeatTime;
+    [SerializeField] float castTimeHard;
+    [SerializeField] float repeatTimeHard;
+    [SerializeField] float RoundTime;
+    private int enemyIndex;
+    private int spawnerIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //InvokeRepeating("GenerateEnemyOne", castTime, repeatTime);
+        StartCoroutine(nameof(ProgressManager));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void GenerateEnemyOne()
+    {
+        enemyIndex = Random.Range(0, enemyPrefabsOne.Length - 1);
+        spawnerIndex = Random.Range(0, spawners.Length );
+        Instantiate(enemyPrefabsOne[enemyIndex], spawners[spawnerIndex].position, Quaternion.identity);
+        //Instantiate(prefab, posicion a generar, rotación con la que se genera)
+    }
+
+    void GenerateEnemyTwo()
+    {
+        enemyIndex = Random.Range(0, enemyPrefabsTwo.Length - 1);
+        spawnerIndex = Random.Range(0, spawners.Length - 1);
+        Instantiate(enemyPrefabsTwo[enemyIndex], spawners[spawnerIndex].position, Quaternion.identity);
+        //Instantiate(prefab, posicion a generar, rotación con la que se genera)
+    }
+
+    void GenerateEnemyThree()
+    {
+        enemyIndex = Random.Range(0, enemyPrefabsThree.Length - 1);
+        spawnerIndex = Random.Range(0, spawners.Length - 1);
+        Instantiate(enemyPrefabsThree[enemyIndex], spawners[spawnerIndex].position, Quaternion.identity);
+        //Instantiate(prefab, posicion a generar, rotación con la que se genera)
+    }
+
+    IEnumerator ProgressManager()
+    {
+        yield return new WaitForSeconds(3f);
+        InvokeRepeating("GenerateEnemyOne", castTime, repeatTime);
+        yield return new WaitForSeconds(RoundTime);
+        CancelInvoke();
+        yield return new WaitForSeconds(5f);
+        InvokeRepeating("GenerateEnemyTwo", castTime, repeatTime);
+        yield return new WaitForSeconds(RoundTime);
+        CancelInvoke();
+        yield return new WaitForSeconds(5f);
+        InvokeRepeating("GenerateEnemyThree", castTimeHard, repeatTimeHard);
+        yield return new WaitForSeconds(RoundTime);
+        CancelInvoke();
+        yield return null;
+    }
+}
